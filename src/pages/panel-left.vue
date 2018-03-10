620ef8c2f5762e81dc395e8d8a3c260613b1f31e
Gabriant <antoniogabriel@outlook.com>
2018-03-10 15:32:47 -0500

    Vue + Webpack + Framework7 instalados.

---
+++ src/pages/panel-left.vue
@@ -0,0 +1,21 @@
+<template>
+  <f7-page>
+    <f7-navbar title="Menu"></f7-navbar>
+    <f7-block strong>
+      <p>Conteúdo de teste</p>
+    </f7-block>
+    <!-- <f7-block-title>Load page in panel</f7-block-title>
+    <f7-list>
+      <f7-list-item link="/about/" title="About"></f7-list-item>
+      <f7-list-item link="/form/" title="Form"></f7-list-item>
+    </f7-list> -->
+    <f7-block-title>Páginas</f7-block-title>
+    <f7-list>
+      <f7-list-item link="/orgaos/" title="Órgãos" view="#main-view" panel-close></f7-list-item>
+      <f7-list-item link="/servicos-online/" title="Serviços Online" view="#main-view" panel-close></f7-list-item>
+    </f7-list>
+  </f7-page>
+</template>
+<script>
+export default {}
+</script>
